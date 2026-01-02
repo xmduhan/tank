e270c73e7351af3ef3cfe7d1bfb41205b1785073
duhan <duhan@189.cn>
2026-01-02 19:54:08 +0800

    .

--- scenes/tank/tank.gd
+++ scenes/tank/tank.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
+# 车身旋转角度（弧度）
+var body_rotation: float = 1.6
+# 炮塔旋转角度（弧度）
+var turret_rotation: float = 0
 
 func _ready():
-	print('------ tank._ready() is called -------')
+	$body.frame = 0
+	$turret.frame = 0
 	
+	# 应用初始旋转角度
+	$body.rotation = body_rotation
+	$turret.rotation = turret_rotation

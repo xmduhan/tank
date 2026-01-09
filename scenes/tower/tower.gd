e959f5eadbd11d15e1db65c574dfb11a94bda779
duhan <duhan@189.cn>
2026-01-09 10:33:08 +0800

    .

--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+var move_speed = 200  # 移动速度，单位：像素/秒
+
+func _process(delta):
+	# 检测键盘输入并移动炮塔
+	if Input.is_action_pressed("tower_left"):  # 默认h键映射到ui_left
+		position.x -= move_speed * delta
+	if Input.is_action_pressed("tower_right"):  # 默认l键映射到ui_right
+		position.x += move_speed * delta

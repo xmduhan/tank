d954cf712b4e36cad92bd39d960c8d1590033130
duhan <duhan@189.cn>
2026-01-10 16:00:02 +0800

    .

--- scripts/units/tank.gd
+++ scripts/units/tank.gd
@@ -3,9 +3,7 @@ extends Vehicle
 class_name Tank
 
 # 坦克特有属性
-# var turret_rotation: float = 0.0  # 炮塔旋转角度
-# var ammo_count: int = 20          # 炮弹数量
-# var max_ammo: int = 20            # 最大炮弹数量
+var turret_rotation: float = 0.0  # 炮塔旋转角度
 
 # 设置炮塔旋转
 func set_turret_rotation(rotation_degrees: float) -> void:

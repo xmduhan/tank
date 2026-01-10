94e3f9eb64438c6ca4bfeb19a5dbfda3a53a12e0
duhan <duhan@189.cn>
2026-01-10 18:48:45 +0800

    .

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 func _ready():
     # 加载坦克场景
-    var tank_scene = load("res://scenes/tank/tank.tscn")
+    var tank_scene = load("res://scenes/units/tank/tank.tscn")
     # 实例化坦克
     var tank_instance = tank_scene.instantiate()
     # 将坦克添加到当前场景
@@ -10,3 +10,6 @@ func _ready():
     # 设置坦克位置为屏幕中心
     tank_instance.position = get_viewport_rect().size / 2
     # tank_instance.position = Vector2(0, 0)
+    
+    # 设置坦克旋转90度
+    print(tank_instance.rotation_degrees)

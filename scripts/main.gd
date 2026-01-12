ff57fc4ec91f4de57ca4a848cbb4557eb0b4d331
duhan <duhan@189.cn>
2026-01-12 16:05:57 +0800

    .

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,16 +4,16 @@ func _ready():
     spawn_enemy_tank(Vector2(570, 180))
     spawn_player_tank(Vector2(200, 180))
 
-func spawn_enemy_tank(position: Vector2):
+func spawn_enemy_tank(_position: Vector2):
     var tank = load("res://scenes/units/tank/enemy.tscn").instantiate()
     add_child(tank)
-    tank.position = position
+    tank.position = _position
     print(tank.rotation_degrees)
     return tank
 
-func spawn_player_tank(position: Vector2):
+func spawn_player_tank(_position: Vector2):
     var player = load("res://scenes/units/tank/player.tscn").instantiate()
     add_child(player)
-    player.position = position
+    player.position = _position
     # player.rotation_degrees = 90  # 旋转90度
     return player

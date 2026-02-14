ee34876166e43079d5a76125c118f0958f92bacf
duhan <duhan@189.cn>
2026-02-14 20:36:40 +0800

    .

--- scenes/units/tank/keyboard.gd
+++ scenes/units/tank/keyboard.gd
@@ -6,16 +6,16 @@ func _ready() -> void:
 	assert(move != null)
 
 func _physics_process(delta: float) -> void:
-	var dir := Vector2.ZERO
+	var direction := Vector2.ZERO
 
 	if Input.is_key_pressed(KEY_H):
-		dir.x -= 1
+		direction.x -= 1
 	if Input.is_key_pressed(KEY_L):
-		dir.x += 1
+		direction.x += 1
 	if Input.is_key_pressed(KEY_K):
-		dir.y -= 1
+		direction.y -= 1
 	if Input.is_key_pressed(KEY_J):
-		dir.y += 1
+		direction.y += 1
 
 	# 防止斜向移动更快
-	move.direction = dir.normalized()
+	move.direction = direction.normalized()

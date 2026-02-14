7cad5e5260e434a41b6d44aea30dd24fba137ff2
duhan <duhan@189.cn>
2026-02-14 20:35:40 +0800

    .

--- scenes/units/tank/keyboard.gd
+++ scenes/units/tank/keyboard.gd
@@ -1,13 +1,11 @@
 extends Node
 
+@onready var move: MoveComponent = get_parent().get_node_or_null("movable") as MoveComponent
+
 func _ready() -> void:
-	pass # Replace with function body.
+	assert(move != null)
 
-
-func _process(delta: float) -> void:
-	pass
-
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	var dir := Vector2.ZERO
 
 	if Input.is_key_pressed(KEY_H):
@@ -18,3 +16,6 @@ func _physics_process(delta):
 		dir.y -= 1
 	if Input.is_key_pressed(KEY_J):
 		dir.y += 1
+
+	# 防止斜向移动更快
+	move.direction = dir.normalized()
